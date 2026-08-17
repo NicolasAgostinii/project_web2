65fd415bc615a51f1aba43f8c6c0c97b6197e5d2
Kaique Cunha Mori <cunhakaique004@gmail.com>
2026-08-17 10:36:28 -0300

    Adicionando a mudanca de varios consultorios para varios produtos

--- src/service/ConsultorioService.java
+++ src/service/ConsultorioService.java
@@ -1,23 +1,25 @@
 package service;
 
+import model.Consulta;
 import model.Paciente;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.xml.crypto.Data;
+import java.util.Date;
 
 @WebService
 @SOAPBinding(style = SOAPBinding.Style.RPC)
 public interface ConsultorioService {
     @WebMethod void adicionarCliente();
+    @WebMethod void adicionarConsultorio();
     @WebMethod void removerCliente();
-    @WebMethod Data data();
-    @WebMethod boolean clientePresente();
+    @WebMethod void removerConsultario();
     @WebMethod String listarClientes();
-    @WebMethod void data1();
+    @WebMethod String listarProdutos();
     @WebMethod void alterarNomeCliente();
-    @WebMethod boolean paceinteExiste(Paciente p );
-    @WebMethod Paciente retornarPaciente(Paciente p);
-
+    @WebMethod void alterarNomeConsultorio();
+    @WebMethod Paciente listarPaciente(Paciente p);
+    @WebMethod Consulta listarConsultas(Consulta c);
 }
