4cf7752afaafd3beec612c3ad55cbea530cc05dc
Kaique Cunha Mori <cunhakaique004@gmail.com>
2026-08-17 09:59:50 -0300

    Ajusta na pasta service

--- src/service/ConsultorioService.java
+++ src/service/ConsultorioService.java
@@ -1,5 +1,18 @@
 package service;
 
-public class ConsultorioService {
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.xml.crypto.Data;
 
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+public interface ConsultorioService {
+    @WebMethod void adicionarCliente();
+    @WebMethod void removerCliente();
+    @WebMethod Data data();
+    @WebMethod boolean clientePresente();
+    @WebMethod String listarClientes();
+    @WebMethod void data1();
+    @WebMethod void alterarNomeCliente();
 }
