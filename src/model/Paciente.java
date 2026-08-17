a2cdd85d586d056c11645af1ef8a167042ea42f7
MarcoMig <151638172+Marco7n@users.noreply.github.com>
2026-08-17 09:57:46 -0300

    Validações Paciente e ConsultorioService para juntar Consultorio e Paciente Service

--- src/model/Paciente.java
+++ src/model/Paciente.java
@@ -26,10 +26,19 @@ public class Paciente {
 	}
 	
 	public String getCpf() {
+		
 		return cpf;
 	}
 	
 	public void setCpf(String cpf) {
+		if(cpf == null || cpf.isBlank()) {
+			throw new IllegalArgumentException("O CPF do cliente é obrigatório");
+		}
+		
+		else if(!cpf.matches("\\d{11}")) {
+			throw new IllegalArgumentException("CPF incompleto");
+		}
+		
 		this.cpf = cpf;
 	}
 	
@@ -38,6 +47,17 @@ public class Paciente {
 	}
 	
 	public void setTelefone(String telefone) {
+		
+		if(telefone == null || telefone.isBlank()) {
+			throw new IllegalArgumentException("O telefone do paciente é obrigatório.");
+		}
+		
+	    String numero = telefone.replaceAll("\\D", "");
+	    
+	    if(numero.length() != 10 && numero.length() != 11) {
+	    	throw new IllegalArgumentException("O telefone deve conter 10 ou 11 números, incluindo o DDD!");
+	    }
+		
 		this.telefone = telefone;
 	}
 	
